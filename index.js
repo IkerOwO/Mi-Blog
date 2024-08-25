9454f653a39bc286081c321a99787ea4c5c260d7
IkerOwO <162556194+IkerOwO@users.noreply.github.com>
2024-08-25 14:04:16 +0200

    First

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("msg").innerHTML = "Hola, soy Iker, y desde pequeño que me encanta la informática. En este blog encontrarás distintos proyectos e ideas que voy teniendo.";
+  });
